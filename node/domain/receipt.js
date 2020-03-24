b35a8fe0c0f4d2558c63515a365181389a2c9271
Chaim Eliyah <chaimeliyah@gmail.com>
2020-03-23 22:45:22 -0700

    naive implementation of api + domain

---
+++ node/domain/receipt.js
@@ -0,0 +1,8 @@
+
+var Receipt = function Receipt(data) {
+  this.loginId = data.loginId;
+  this.email = data.email;
+  this.datetime = data.datetime;
+}
+
+module.exports = Receipt;
---
+++ node/domain/schemas.js
@@ -0,0 +1,18 @@
+/** schemas.js **/
+
+schemas = {
+  login: {
+    id: null,
+    email: null,
+    datetime: null
+  },
+  receipt: {
+    id: null,
+    loginId: null,
+    email: null,
+    datetime: null
+  }
+}
+
+module.exports = schemas;
+
