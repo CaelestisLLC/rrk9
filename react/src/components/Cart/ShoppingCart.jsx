c90ee0ff75a0aa0c2a8f9b45e5f84a51720822ba
Chaim Eliyah <chaimeliyah@gmail.com>
2020-04-12 21:44:18 -0700

    Feature/5 -> master (#29)
    * update store to be reactive based on tags, selected by tab
    
    * add user email when login clicked. also added fb and google imgs but didn't use.
    
    * add facebook, google, logout links. add icons.
    fix css for main app. fix css for shopping cart.
    fix error messages for console.
    RESOLVE #5.
    
    Co-authored-by: Liu Ying <dearlittlebird@gmail.com>

--- react/src/components/Cart/ShoppingCart.jsx
+++ react/src/components/Cart/ShoppingCart.jsx
@@ -5,47 +5,49 @@ import './ShoppingCart.css';
 import Fakes from '../../fakes.js';
 
 // https://medium.com/@ayabellazreg/make-a-simple-shopping-cart-app-using-react-redux-1-3-fefde93e80c7
-function ShoppingCart() {
+class ShoppingCart extends React.Component {
 
-  let cart = { total: Fakes.cartItemArray.reduce((sum, cartItem) => { return sum + (cartItem.quantity * cartItem.price); }, 0) };
+  render () {
+    let cart = { total: Fakes.cartItemArray.reduce((sum, cartItem) => { return sum + (cartItem.quantity * cartItem.price); }, 0) };
 
-  return (
-    <div className="ShoppingCart">
-      <div className="Cart-container">
-        {Fakes.cartItemArray.map(item => 
-          <div className={"Cart-item-" + item.key} key={item.key}>
+    return (
+      <div className="ShoppingCart">
+        <div className="Cart-container">
+          {Fakes.cartItemArray.map(item => 
+            <div className={"Cart-item-" + item.key} key={item.key}>
 
-            <div className={"Item-image-container-" + item.key}>
-              <img src={item.image} className={"Item-image-" + item.key} alt={item.alt} />
-            </div>
+              <div className={"Item-image-container-" + item.key}>
+                <img src={item.image} className={"Item-image-" + item.key} alt={item.alt} />
+              </div>
 
-            <div className={"Item-description-container-" + item.key}>
-              <span className={"Item-description-" + item.key}>
-                {item.name}
-              </span>
-              <span className={"Item-price-" + item.price}>
-                Cost: {item.price /* todo: dropdown */}
-              </span>
-              <span className={"Item-quantity-" + item.key}>
-                Qty: {item.quantity /* todo: dropdown */}
-              </span>
-              <span className={"Item-total-" + item.key}>
-                Subtotal: {"$" + (item.quantity * item.price).toFixed(2) /* todo: dropdown */}
-              </span>
-            </div>
-          </div>      
-        )}
+              <div className={"Item-description-container-" + item.key}>
+                <span className={"Item-description-" + item.key}>
+                  {item.name}
+                </span>
+                <span className={"Item-price-" + item.price}>
+                  Cost: {item.price /* todo: dropdown */}
+                </span>
+                <span className={"Item-quantity-" + item.key}>
+                  Qty: {item.quantity /* todo: dropdown */}
+                </span>
+                <span className={"Item-total-" + item.key}>
+                  Subtotal: {"$" + (item.quantity * item.price).toFixed(2) /* todo: dropdown */}
+                </span>
+              </div>
+            </div>      
+          )}
+        </div>
+        <div className="Cart-total">
+          <span>
+            {"Total: $" + cart.total.toFixed(2)}
+          </span>
+        </div>
+        <div className="Paypal">
+          <img src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/checkout-logo-large.png" alt="Check out with PayPal" />
+        </div>
       </div>
-      <div className="Cart-total">
-        <span>
-          {"Total: $" + cart.total.toFixed(2)}
-        </span>
-      </div>
-      <div className="Paypal">
-        <img src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/checkout-logo-large.png" alt="Check out with PayPal" />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ShoppingCart;
--- react/src/fakes.js
+++ react/src/fakes.js
@@ -17,10 +17,21 @@ let cartItem1 = { key: Math.floor(Math.random() * 1000), name: "Zignature Brand 
 let cartItem2 = { key: Math.floor(Math.random() * 1000), name: "Zignature Brand Dog Food", quantity: 2, price: 69.99, image: item_example, alt: "Zignature" };
 let cartItem3 = { key: Math.floor(Math.random() * 1000), name: "Zignature Brand Dog Food", quantity: 1, price: 69.99, image: item_example, alt: "Zignature" };
 
+let storeItem1 = { key: Math.floor(Math.random() * 1000), name: "Zignature Brand Dog Food", image: item_example, alt: "Zignature", tags: ['shop'] };
+let storeItem2 = { key: Math.floor(Math.random() * 1000), name: "Zignature Brand Dog Food", image: item_example, alt: "Zignature", tags: ['shop', 'deals'] };
+let storeItem3 = { key: Math.floor(Math.random() * 1000), name: "Zignature Brand Dog Food", image: item_example, alt: "Zignature", tags: ['shop', 'deals', 'clearance'] };
+
+let tab0 = { key: "Shop" }
+let tab1 = { key: "Deals" }
+let tab2 = { key: "Clearance" }
+
 class Fakes extends React.Component { }
 
 Fakes.menuListArray = [ list0, list1, list2 ];
 Fakes.footerLinkArray = [ link1, link2 ];
-Fakes.cartItemArray = [ cartItem1, cartItem2, cartItem3 ]
+Fakes.cartItemArray = [ cartItem1, cartItem2, cartItem3 ];
+Fakes.storeItemArray = [ storeItem1, storeItem2, storeItem3 ];
+Fakes.tabItemArray = [ tab0, tab1, tab2 ];
+Fakes.userEmail = "bernie@sanders.org";
 
 export default Fakes;
