246baf033deb3f4b3eeb69e98d1421088e221abb
Chaim Eliyah <chaimeliyah@gmail.com>
2020-04-29 19:56:36 -0700

    Naive implemention of PayPal button to get dearlittlebird going

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 [![VerbalWebsites](https://circleci.com/gh/VerbalWebsites/rrk9-store.svg?style=svg)](https://app.circleci.com/pipelines/github/VerbalWebsites/rrk9-store)
 
 MIT License. All original material (c) 2020 Chaim Eliyah and Ying Liu.
+
+### React App
+For the React app, the following environment variables need to be set:
+```
+# bash
+export REACT_APP_SANDBOX_CLIENT_ID=${value}
+export REACT_APP_PRODUCTION_CLIENT_ID=${value} # even if not production value
+```
+...prior to running `npm start`
---
+++ react/src/components/PayPal/Button.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PayPal from 'paypal-checkout';
+
+import './Button.css';
+
+class PayPalButton extends React.Component {
+
+  onAuthorize(data, actions) {
+    console.log('Payment was successful!', data, actions);
+    // TODO: add confirmation notice/spinner
+    // this.ordersService.executePaypal(this.order.id, data['payerID'], data['paymentID'], data['paymentToken'])
+    //   .subscribe(
+    //     data => {
+    //       console.log('execute paypal returned:', data);
+    //       this.ordersService.getOrder(this.order.id)
+    //         .subscribe(foo => this.order = foo);
+    //     },
+    //     error => console.error('execute paypal errored:', error));
+  }
+  onCancel(data, actions) {
+    console.log('The payment was cancelled!', data);
+  }
+  onError(data, actions) {
+    console.log('There was an error:', data);
+  }
+  payment(data, actions) {
+    return actions.payment.create({
+      payment: {
+        transactions: [
+          { amount: { total: this.transportCost, currency: 'USD' } }
+        ]
+      // },
+      // experience: {
+      //   input_fields: {
+      //       no_shipping: 1
+      //   }
+      }
+    });
+  }
+
+  render () {
+    let Button = PayPal.Button.driver('react', { React, ReactDOM });
+
+    return (
+      //   <img src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/checkout-logo-large.png" alt="Check out with PayPal" />
+      <div className="Paypal">
+        <Button
+          // createOrder={ (data, actions) => this.createOrder(data, actions) }
+          // onApprove={ (data, actions) => this.onApprove(data, actions) }
+          env='sandbox'
+          client={ { sandbox: process.env.REACT_APP_SANDBOX_CLIENT_ID, production: process.env.REACT_APP_PRODUCTION_CLIENT_ID } }
+          payment={ (data, actions) => this.payment(data, actions) }
+          commit={true}
+          onAuthorize={ (data, actions) => this.onAuthorize(data, actions) }
+          onCancel={ (data, actions) => this.onCancel(data, actions) }
+          onError={ (data, actions) => this.onError(data, actions) }
+        />
+      </div>
+    );
+  }
+}
+
+export default PayPalButton;
